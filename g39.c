0010590035ebec05913f02d22e6e9ddfa91745e5
Rajath K Naik <rajathkrishnamurthy173@gmail.com>
2024-11-11 20:11:48 +0530

    Create g39.c

---
+++ g39.c
@@ -0,0 +1,61 @@
+/*Imagine you're creating an age and gender categorization system where participants need to provide their age and gender during registration. 
+
+
+
+The system should then determine whether they are a male or female adult (age 18 or older) or a male or female minor (under 18 years old). 
+
+
+
+Write a program that takes age and gender as input and categorizes participants accordingly.
+
+Input format :
+The first line of input consists of an integer representing the age.
+
+The second line of input consists of a character representing the gender.
+
+
+
+Note: m/M for male; f/F for female
+
+Output format :
+If the age is 18 or above:
+
+If the gender is M/m, the output prints "You are a male adult."
+If the gender is F/f', the output prints "You are a female adult."
+
+
+If the age is below 18:
+
+If the gender is M/m, the output prints "You are a male minor."
+If the gender is F/f, the output prints "You are a female minor."
+
+
+For any other gender input, the output prints "Invalid gender input."*/
+
+#include <stdio.h>
+
+int main() {
+    int age;
+    char gender;
+    scanf("%d", &age);
+    scanf(" %c", &gender);
+    if (age >= 18) {
+        if (gender == 'M' || gender == 'm') {
+            printf("You are a male adult.");
+        } else if (gender == 'F' || gender == 'f') {
+            printf("You are a female adult.");
+        } else {
+            printf("Invalid gender input.");
+        }
+    } else {
+        if (gender == 'M' || gender == 'm') {
+            printf("You are a male minor.");
+        } else if (gender == 'F' || gender == 'f') {
+            printf("You are a female minor");
+        } else {
+            printf("Invalid gender input");
+        }
+    }
+    return 0;
+}
+
